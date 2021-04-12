5ee7c57808d9aa367d3d7bcd6919652340f67811
jaradat-pdb <admin@pdbcorp.org>
2021-04-11 21:01:03 -0700

    Refactor code and reorganize project structure

--- pms3/server/app.js
+++ pms3/server/app.js
@@ -1,9 +1,8 @@
 const port = 3000;
 const express = require('express');
 const app = express();
-const fileServerMiddleware = express.static('public');
 
-app.use('/', fileServerMiddleware);
+app.use('/public', express.static('public'));
 app.listen(port, () => {
     console.log(`Server started and listening on port ${port}`);
 });
