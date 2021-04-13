fa2195bada518fdea74b2cb5be333bbc2331da5a
jaradat-pdb <admin@pdbcorp.org>
2021-04-13 11:50:49 -0700

    Add babel config file to specifiy presets and plugins and reduce amount of typing in command line

--- pms3/public/app.js
+++ pms3/public/app.js
@@ -1,7 +1,9 @@
-const elementDiv = React.createElement('div', {
+"use strict";
+
+var elementDiv = React.createElement('div', {
   title: 'Element div'
 }, [React.createElement('h1', null, 'Hola Mundo'), React.createElement('h2', null, 'que te pasa putas!')]);
-const jsxDiv = /*#__PURE__*/React.createElement("div", {
+var jsxDiv = /*#__PURE__*/React.createElement("div", {
   title: "JSX div"
 }, /*#__PURE__*/React.createElement("h3", null, "por que muy serioso?"));
 ReactDOM.render(jsxDiv, document.getElementById('jsxDiv'));
