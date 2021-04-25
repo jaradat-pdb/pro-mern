317a5b7551c6e94076f67fd76dd6b08fa2837f47
jaradat-pdb <admin@pdbcorp.org>
2021-04-25 15:28:39 -0700

    Add new IssueRow class concerned with constructing the individual rows in the IssueTable component, along with some formatting changes

--- pms3/public/issues.js
+++ pms3/public/issues.js
@@ -82,24 +82,66 @@ var IssueTable = /*#__PURE__*/function (_React$Component3) {
   _createClass(IssueTable, [{
     key: "render",
     value: function render() {
-      return /*#__PURE__*/React.createElement("div", {
-        title: "Table issue div"
-      }, "This is a placeholder for a table of issues.");
+      var rowStyle = {
+        border: "1px solid silver",
+        padding: 4
+      };
+      return /*#__PURE__*/React.createElement("table", {
+        style: {
+          borderCollapse: "collapse"
+        }
+      }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", {
+        style: rowStyle
+      }, "ID"), /*#__PURE__*/React.createElement("th", {
+        style: rowStyle
+      }, "Title"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement(IssueRow, {
+        rowStyle: rowStyle,
+        issue_id: 1
+      }, "Error in console when clicking Add"), /*#__PURE__*/React.createElement(IssueRow, {
+        rowStyle: rowStyle,
+        issue_id: 2
+      }, "Missing ", /*#__PURE__*/React.createElement("b", null, "bottom"), " border on panel")));
     }
   }]);
 
   return IssueTable;
 }(React.Component);
 
-var Issues = /*#__PURE__*/function (_React$Component4) {
-  _inherits(Issues, _React$Component4);
+var IssueRow = /*#__PURE__*/function (_React$Component4) {
+  _inherits(IssueRow, _React$Component4);
 
-  var _super4 = _createSuper(Issues);
+  var _super4 = _createSuper(IssueRow);
+
+  function IssueRow() {
+    _classCallCheck(this, IssueRow);
+
+    return _super4.apply(this, arguments);
+  }
+
+  _createClass(IssueRow, [{
+    key: "render",
+    value: function render() {
+      var style = this.props.rowStyle;
+      return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+        style: style
+      }, this.props.issue_id), /*#__PURE__*/React.createElement("td", {
+        style: style
+      }, this.props.children));
+    }
+  }]);
+
+  return IssueRow;
+}(React.Component);
+
+var Issues = /*#__PURE__*/function (_React$Component5) {
+  _inherits(Issues, _React$Component5);
+
+  var _super5 = _createSuper(Issues);
 
   function Issues() {
     _classCallCheck(this, Issues);
 
-    return _super4.apply(this, arguments);
+    return _super5.apply(this, arguments);
   }
 
   _createClass(Issues, [{
--- pms3/src/jsx/issues.jsx
+++ pms3/src/jsx/issues.jsx
@@ -16,8 +16,32 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
     render() {
+        const rowStyle = {border: "1px solid silver", padding: 4};
         return (
-            <div title="Table issue div">This is a placeholder for a table of issues.</div>
+            <table style={{borderCollapse: "collapse"}}>
+                <thead>
+                    <tr>
+                        <th style={rowStyle}>ID</th>
+                        <th style={rowStyle}>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow rowStyle={rowStyle} issue_id={1}>Error in console when clicking Add</IssueRow>
+                    <IssueRow rowStyle={rowStyle} issue_id={2}>Missing <b>bottom</b> border on panel</IssueRow>
+                </tbody>
+            </table>
+        );
+    };
+}
+
+class IssueRow extends React.Component {
+    render() {
+        const style = this.props.rowStyle;
+        return (
+            <tr>
+                <td style={style}>{this.props.issue_id}</td>
+                <td style={style}>{this.props.children}</td>
+            </tr>
         );
     };
 }
