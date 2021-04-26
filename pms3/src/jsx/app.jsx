b2a0033bcbffd58710af185c9dcac011da289243
jaradat-pdb <admin@pdbcorp.org>
2021-04-26 06:44:27 -0700

    Add issues array to serve as temporary in-memory db

--- pms3/src/jsx/app.jsx
+++ pms3/src/jsx/app.jsx
@@ -1,3 +1,24 @@
+const issues = [
+    {
+        id: 1,
+        status: 'New',
+        owner: 'Waleed',
+        effort: 5,
+        created: new Date('2021-04-20'),
+        due: undefined,
+        title: 'Error in console when clicking Add'
+    },
+    {
+        id: 2,
+        status: 'Assigned',
+        owner: 'Nasser',
+        effort: 7,
+        created: new Date('2021-04-18'),
+        due: new Date('2021-05-01'),
+        title: 'Missing bottom border on panel'
+    }
+];
+
 class HelloContinentsComponent extends React.Component {
     render() {
         const continents = [
